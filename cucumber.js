42285b0a40c52e770d7de72781d485eb331c2415
sanaz <miss.abbaszade@gmail.com>
2025-09-13 17:28:38 +0330

    Fix some test failures

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,11 @@
+
 module.exports = {
-  default: "--publish-quiet",
+  default: {
+    require: ["./features/step_definitions/*.js"],
+    format: ["progress", "html:cucumber-report.html"],
+    publishQuiet: false,
+    publishEnabled: false 
+  }
 }
 //npx cucumber-js features/ErrorValidation.feature --exit
 //npx cucumber-js --parallel 2 --exit --format html:cucumber-report.html
--- playwright.config.js
+++ playwright.config.js
@@ -3,26 +3,29 @@ const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',
-  retries :0,
-  
+  retries: 0,
+
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-  
+
     timeout: 5000
   },
-  
-  reporter: 'html',
+
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
-    browserName : 'chromium',
-    headless : true,
-    screenshot : 'on',
-    trace : 'on',//off,on
-    
-    
-    
+    browserName: 'chromium',
+    headless: false,
+    screenshot: 'on',
+    trace: 'on',//off,on
+    video: 'on',
+
+
   },
 
 
